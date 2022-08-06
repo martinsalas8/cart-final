cc2dfd2b0cdd8dd238cadb5afc0d9963a1e7150b
dasdo <jmsalascastillo@gmail.com>
2022-08-06 18:18:25 -0500

    proyecto subido

---
+++ js/pedido.js
@@ -0,0 +1,44 @@
+// /// inicio fetch
+
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+// 		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=10&numberOfTopResults=5', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+// /// fin fetch
+
+const carro = new Carrito();
+const carrito = document.getElementById('carrito');
+const productos = document.getElementById('lista-productos');
+const listaProductos = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+const procesarPedidoBtn = document.getElementById('procesar-pedido');
+
+cargarEventos();
+
+function cargarEventos(){
+
+    //Se ejecuta cuando se presionar agregar carrito
+    productos.addEventListener('click', (e)=>{carro.comprarProducto(e)});
+
+    //Cuando se elimina productos del carrito
+    carrito.addEventListener('click', (e)=>{carro.eliminarProducto(e)});
+
+    //Al vaciar carrito
+    vaciarCarritoBtn.addEventListener('click', (e)=>{carro.vaciarCarrito(e)});
+
+    //Al cargar documento se muestra lo almacenado en LS
+    document.addEventListener('DOMContentLoaded', carro.leerLocalStorage());
+
+    //Enviar pedido a otra pagina
+    procesarPedidoBtn.addEventListener('click', (e)=>{carro.procesarPedido(e)});
+}
